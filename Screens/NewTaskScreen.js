1f0f3b5c959d2a825be6b1178bc896c18fbe1507
Kamil <unnamedxaer@gmail.com>
2020-02-13 00:01:26 +0100

    Complete New Task Screen UI

--- Screens/NewTaskScreen.js
+++ Screens/NewTaskScreen.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, ScrollView, TextInput, Platform, Button } from 'react-native';
-import DatePicker from '@react-native-community/datetimepicker';
-import Header from '../Components/Header';
+import { View, Text, StyleSheet, ScrollView, TextInput, Platform, Picker, Dimensions } from 'react-native';
 import ImportanceLevel from '../Constants/ImportanceLevels';
 import Colors from '../Constants/Colors';
 import moment from 'moment';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { TouchableOpacity, TouchableNativeFeedback } from 'react-native-gesture-handler';
-
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
+
+const screenWidth = Dimensions.get('screen').width;
+
 
 const NewTaskScreen = (props) => {
     const TouchableComponent = Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity;
@@ -27,7 +27,7 @@ const NewTaskScreen = (props) => {
 
     const cancelDatePickerHandler = () => {
         setDatePickerVisibility(false);
-    }
+    };
 
     const confirmDatePickerHandler = date => {
         console.log("A date has been picked: ", moment(remindDate).format('dddd, DD MMM YYYY, HH:mm'));
@@ -38,123 +38,148 @@ const NewTaskScreen = (props) => {
 
     const clearRemindDateHandler = (ev) => {
         setIsRemindDateSet(false);
-    }
+    };
 
     return (
-        <ScrollView style={styles.screen}>
-            <Header>Add New Task</Header>
-            <Text style={styles.label}>Activity Title</Text>
-            <TextInput
-                style={styles.input}
-                placeholder="Activity in few words..."
-                numberOfLines={2}
-                maxLength={50}
-                value={title}
-                nativeID="title"
-                onChangeText={setTitle}
-                autoCompleteType="off"
-                // autoFocus={true}
-                importantForAutofill="no"
-            />
-            <Text style={styles.label}>Activity Description</Text>
-            <TextInput
-                style={styles.input}
-                placeholder="Put some details here..."
-                multiline={true}
-                numberOfLines={3}
-                maxFontSizeMultiplier={1}
-                maxLength={500}
-                value={description}
-                nativeID="description"
-                caretHidden={false}
-                onChangeText={setDescription}
-                autoCompleteType="off"
-                textAlignVertical="top"
-            />
-            
+        <ScrollView
+            style={styles.screen}
+        >
+            <View>
+                <Text style={styles.label}>Activity title</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Activity in few words..."
+                    numberOfLines={2}
+                    maxLength={50}
+                    value={title}
+                    nativeID="title"
+                    onChangeText={setTitle}
+                    autoCompleteType="off"
+                    autoFocus={true}
+                    importantForAutofill="no"
+                    returnKeyType="next"
+                />
+                <Text style={styles.inputInfo}>{title.length}/50 characters</Text>
+            </View>
+            <View>
+                <Text style={styles.label}>Activity description</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Put some details here..."
+                    multiline={true}
+                    numberOfLines={3}
+                    maxFontSizeMultiplier={1}
+                    maxLength={500}
+                    value={description}
+                    nativeID="description"
+                    caretHidden={false}
+                    onChangeText={setDescription}
+                    autoCompleteType="off"
+                    textAlignVertical="top"
+                />
+                <Text style={styles.inputInfo}>{description.length}/500 characters</Text>
+            </View>
+            <View>
+                <View style={styles.input}>
+                    <Picker
+                        prompt="Importance"
+                        selectedValue={imporntance}
+                        mode='dialog'
+                        onValueChange={(itemValue, _) => setImporntance(itemValue)}>
+                        {Object.keys(ImportanceLevel).map(x => <Picker.Item key={x} label={ImportanceLevel[x]} value={x} />)}
+                    </Picker>
+                </View>
+                <Text style={styles.inputInfo}>Select a level of importance</Text>
+            </View>
+
             <View style={styles.remindDateWrapper}>
-                <Text style={styles.label}>Remind me at: </Text>
-                <TouchableComponent
-                    onPress={showDatePickerHandler}>
-                    <View style={styles.remindDateButtonsWrapper}>
+                <Text style={styles.label}>Remind at</Text>
+                <View style={styles.remindDateButtonsWrapper}>
+                    <TouchableComponent
+                        onPress={showDatePickerHandler}>
                         <Text
-                            style={styles.remindDateButtonText}>
+                            style={{ ...styles.remindDateButtonText, ...{ color: `rgba(0, 0, 0, ${!isRemindDateSet ? 0.35 : 1})` } }}>
                             {isRemindDateSet
                                 ? moment(remindDate).format('dddd, DD MMM YYYY, HH:mm')
                                 : 'Pick a time...'}
                         </Text>
-                        {isRemindDateSet && <View style={styles.clearButton}>
-                            <Button onPress={clearRemindDateHandler} title="ttt">
-                                <MaterialCommunityIcons
-                                    name="eraser"
-                                    size={28}
-                                    color={Colors.primary}
-                                />
-                            </Button>
-                        </View>}
-                    </View>
-                </TouchableComponent>
-            </View>
+                    </TouchableComponent>
 
-            <DateTimePickerModal
-                date={remindDate}
-                isVisible={isDatePickerVisible}
-                mode="datetime"
-                onConfirm={confirmDatePickerHandler}
-                onCancel={cancelDatePickerHandler}
-            />
-        </ScrollView>
-    )
-}
+                    {
+                        isRemindDateSet &&
+                        <TouchableComponent
+                            style={styles.clearButton}
+                            onPress={clearRemindDateHandler}>
+                            <MaterialCommunityIcons
+                                name="eraser"
+                                size={28}
+                                color={Colors.primary}
+                            />
+                        </TouchableComponent>
+                    }
+                </View>
+                <DateTimePickerModal
+                    date={remindDate}
+                    isVisible={isDatePickerVisible}
+                    mode="datetime"
+                    onConfirm={confirmDatePickerHandler}
+                    onCancel={cancelDatePickerHandler}
+                />
+            </View>
+        </ScrollView >
+    );
+};
 
 const styles = StyleSheet.create({
     screen: {
-        flex: 1
+        flex: 1,
+        paddingHorizontal: screenWidth < 300 ? 5 : screenWidth < 600 ? 15 : 30
     },
     label: {
         marginTop: 10,
-        marginHorizontal: 20
+        marginHorizontal: 20,
+        fontWeight: 'bold'
+    },
+    inputInfo: {
+        fontSize: 10,
+        fontWeight: 'bold',
+        color: 'rgba(0, 0, 0, 0.35)',
+        paddingHorizontal: 5
     },
     input: {
-        borderColor: Colors.pDark,
-        borderWidth: 2,
+        borderColor: Colors.secondary,
+        borderWidth: 0,
+        borderBottomWidth: 2,
         paddingHorizontal: 16,
-        paddingVertical: 5,
+        paddingTop: 5,
         margin: 5
     },
+
     remindDateWrapper: {
         flexDirection: 'column',
     },
     remindDateButtonsWrapper: {
-
-        elevation: 3,
-        shadowColor: "#ccc",
-        shadowOffset: { width: 0, height: 4 },
-        shadowOpacity: 10,
-        shadowRadius: 10,
-        backgroundColor: 'white',
-
         flexDirection: 'row',
         justifyContent: 'center',
-        alignItems: 'center',
+        alignItems: 'stretch',
         margin: 5
-
     },
     remindDateButtonText: {
-        fontSize: 18,
-        marginVertical: 16,
+        fontSize: 16,
+        marginVertical: 10,
         paddingHorizontal: 10,
-        borderBottomColor: Colors.secondary,
-        borderWidth: 2,
+        paddingBottom: 5,
+        borderWidth: 0,
         borderColor: Colors.secondary,
-        borderWidth: 2,
-        borderTopWidth: 0,
-        borderLeftWidth: 0,
-        borderRightWidth: 0
+        borderBottomWidth: 2,
+        minWidth: 200,
+        textAlign: 'center'
     },
     clearButton: {
-        marginHorizontal: 10, zIndex: 100
+        padding: 10,
+        marginHorizontal: 6,
+        justifyContent: 'center',
     }
-})
+});
 
 export default NewTaskScreen;

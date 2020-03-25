002f4be0ea9e34cb1207d43e174c87cea4de8f33
Kamil <unnamedxaer@gmail.com>
2020-03-25 20:51:51 +0100

    Clear file

--- axios/axios.js
+++ axios/axios.js
@@ -1,7 +1,8 @@
 import Axios from 'axios';
+import { API_URL } from '../env';
 
 const axios = Axios.create({
-    baseURL: 'https://rn-tasks.firebaseio.com',
+    baseURL: API_URL,
 });
 
 export default axios;
